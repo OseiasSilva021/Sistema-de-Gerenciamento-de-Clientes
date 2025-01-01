5a652b8437d2831101de60ca6c4a8e37621721bb
Oseias <soseias894@gmail.com>
2025-01-01 16:18:01 -0300

    agora as senhas dos usuários são encriptadas ao serem salvas no banco de dados

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,4 +1,4 @@
-const { required } = require('joi');
+const bcrypt = require('bcrypt')
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
@@ -32,6 +32,21 @@ let UserSchema = new Schema({
         required: false
     }
 })
+
+UserSchema.pre('save', async function (next) {
+
+    if(!this.isModified('password')){
+        return next()
+    }
+    const salt = await bcrypt.genSalt(10)
+    this.password = await bcrypt.hash(this.password, salt)
+    next()
+})
+
+UserSchema.methods.matchPassword = async function (enteredPassword){
+    return await bcrypt.compare(enteredPassword, this.password)
+}
+
 
 const User = mongoose.model('User', UserSchema)
 module.exports = User
